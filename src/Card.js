9b0f67e205fe0e3441f331b3dfeb1943f62d8a8b
Sean Cherry <seandavidcherry@gmail.com>
2022-05-14 16:23:21 -0500

    init project

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import styled from "styled-components";
+import "tachyons";
+//import { robots } from "./robots";
+
+const card = ({ id, name, email }) => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default card;
